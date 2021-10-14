ca3fa226f8df5f2dc5448997a8e06cd1117d8570
juanxo13597 <juanxo97master@gmail.com>
2021-10-14 16:15:16 +0200

    adapto de base de datos

--- app/Models/Fallecido.php
+++ app/Models/Fallecido.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class History extends Model
+class Fallecido extends Model
 {
     use HasFactory;
 
-    protected $table = "histories";
+    protected $table = "fallecidos";
 }
