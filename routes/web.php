0136e6cce1192911d683a46671c5511de93786b0
juanxo13597 <juanxo97master@gmail.com>
2021-10-18 13:28:06 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,5 @@ use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\FallecidoController;
 
 
-// rutas de usuario
-Route::get('/api/usuarios', [UsuarioController::class, 'lista']);
-
-
 //rutas historiales
 Route::get('/api/fallecidos', [FallecidoController::class, 'ultimos_fallecidos']);
